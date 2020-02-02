cf139245473e40069330e8a13ad924c90845f688
Swaraj <swaraj.k@everest-ims.com>
2020-02-03 02:44:50 +0530

    extra filter removed

--- bankapi/settings.py
+++ bankapi/settings.py
@@ -71,7 +71,6 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'SEARCH_PARAM': 'q',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
